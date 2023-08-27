75e83f666ee47f50b056cabe39697a3a9f9620e5
Daniel Ivanov <daniel.ivanov@me.com>
2023-08-27 15:52:01 +0200

    - added `wasm` loaders to exports;

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import WALoader from './wasm-helpers/vector_store_helpers'
+
 export * from './database'
 export * from './vector-store'
 export * from './embeddings-strategies'
 
+export { WALoader }
 export { VectorStore } from './store'
